a5e6a972e66bd3aa14625dcdfb57293a1d52beba
pgautrea <pierregautreau@outlook.com>
2020-07-27 23:40:02 +0200

    Ajout build

---
+++ ros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pgautrea/cdfr_ros/ros_ws/src
+# Build directory: /home/pgautrea/cdfr_ros/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_2W")
---
+++ ros_ws/build/robot_2W/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pgautrea/cdfr_ros/ros_ws/src/robot_2W
+# Build directory: /home/pgautrea/cdfr_ros/ros_ws/build/robot_2W
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

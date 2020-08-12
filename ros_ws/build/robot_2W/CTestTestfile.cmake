4328529c5bc90be86ce830c0cd851c52a719d9bf
pgautrea <pierregautreau@outlook.com>
2020-08-12 19:46:18 +0200

    cleaning repo, modifying gitignore, robot model is showing in rviz and gazebo, adding xacro file for robot description

--- ros_ws/build/robot_2W/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pgautrea/cdfr_ros/ros_ws/src/robot_2W
-# Build directory: /home/pgautrea/cdfr_ros/ros_ws/build/robot_2W
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
